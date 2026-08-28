537e759f7e41ec00f12bdca77d46b3f50a80190b
MK1603 <saikrishnanmk1603@gmail.com>
2026-08-28 08:52:56 +0530

    fix(web): sync legacy routes /ceo/projects/new and /co-ceo/projects/new to render ProjectCreationWorkspace

--- apps/web/app/(organization)/co-ceo/projects/new/page.tsx
+++ apps/web/app/(organization)/co-ceo/projects/new/page.tsx
@@ -3,6 +3,6 @@
 import React from "react";
 import { ProjectCreationWorkspace } from "@/components/projects/project-creation-workspace";
 
-export default function NewProjectWizardPage() {
-  return <ProjectCreationWorkspace userRole="CEO" basePath="/ceo" />;
+export default function COCEOProjectNewPage() {
+  return <ProjectCreationWorkspace userRole="CO-CEO" basePath="/co-ceo" />;
 }
